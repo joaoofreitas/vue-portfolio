86fc8078fc88eb2a95462e730b6f0f5d12dd72b2
joaoofreitas <joaoofreitas00@gmail.com>
2020-09-19 23:07:24 +0100

    API Auth Done. Need to parse data.

--- portfolio/backend/requests.js
+++ portfolio/backend/requests.js
@@ -1,3 +1,25 @@
-const test = "Not an Hello World"
+const axios = require('axios').default;
+require('dotenv').config()
 
-export default test
+axios.get('https://api.github.com/users/joaoofreitas/repos', { 'headers': { 'Authorization': process.env.TOKEN } })
+    .then((response) => {
+    // handle success
+    console.log(response);
+  })
+    .catch((error) => {
+    // handle error
+    console.log(error);
+  })
+    .then(() => {
+    // always executed
+  });
+  
+
+  /* Stuff to fetch:
+    response.
+        name
+        html_url
+        description
+        languages_url
+
+  */
