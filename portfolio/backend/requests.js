6b3ef325a83e93ff736c32ca591098cd485b9b48
joaoofreitas <joaoofreitas00@gmail.com>
2020-09-20 23:28:26 +0100

    Storing API Stuff to Object, Done.

--- portfolio/backend/requests.js
+++ portfolio/backend/requests.js
@@ -1,17 +1,27 @@
 const axios = require('axios').default;
 require('dotenv').config()
 
+var repositories = {
+  name: [],
+  html_url: [],
+  description: [],
+  languages_url: []
+
+};
+
 axios.get('https://api.github.com/users/joaoofreitas/repos', { 'headers': { 'Authorization': process.env.TOKEN } })
-    .then((response) => {
-    // handle success
-    console.log(response);
-  })
-    .catch((error) => {
-    // handle error
-    console.log(error);
-  })
-    .then(() => {
-    // always executed
+  .then((response) => {
+    for (i = 0; i < response.data.length; i++) {
+      if (response.data[i].description != null) {
+        repositories.name.push(response.data[i].name)
+        repositories.html_url.push(response.data[i].html_url)
+        repositories.description.push(response.data[i].description)
+        repositories.languages_url.push(response.data[i].languages_url)
+      } else {
+        console.log(`The repositories ignored are: ${response.data[i].name}`)
+      }
+    }
+    console.log(repositories);
   });
   
 
